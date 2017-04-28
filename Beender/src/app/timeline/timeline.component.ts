39d09891b3eec08a4427191ef591e5f86cfc20ec
Micha Molko <Micha Molko>
2017-04-28 10:06:34 +0300

    added connection to server

--- Beender/src/app/timeline/timeline.component.ts
+++ Beender/src/app/timeline/timeline.component.ts
@@ -5,6 +5,7 @@ import { UserService } from "app/user.service";
 import { ActivatedRoute,Params } from "@angular/router";
 import 'rxjs/add/operator/switchMap';
 import { Timeline } from "app/timeline";
+import { Observable } from "rxjs/Observable";
 
 // interface marker {
 // 	lat: number;
@@ -17,7 +18,7 @@ import { Timeline } from "app/timeline";
   styleUrls: ['./timeline.component.css']
 })
 export class TimelineComponent implements OnInit {
-   @Input() timeline : Timeline ;
+   timeline : Timeline ;
   // lat: number = 51.678418;
   // lng: number = 7.809007;
   constructor(private service: UserService,
@@ -48,7 +49,10 @@ export class TimelineComponent implements OnInit {
   ngOnInit() {
     this.route.params.subscribe((params: Params) => 
          {
-           this.timeline = this.service.getTimeline(+params["user_id"],+params["timeline_id"]);
+          this.service.getTimeline(params["user_id"],+params["timeline_id"]).subscribe(
+            (timeline:Timeline)=>this.timeline = timeline
+          );
+          //  console.log(this.timeline);
           //  for(var i in this.timeline.coords){
           //    this.markers.push({lat:this.timeline.coords[i][0],
           //      lng:this.timeline.coords[i][1],
